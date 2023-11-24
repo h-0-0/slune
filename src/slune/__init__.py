15429482b99f039904acdd729d57fdb976003a75
h-aze <hwbourne@gmail.com>
2023-11-24 12:08:54 +0000

    Now can import from loggers, savers and searchers directories more nicely, eg. from slune.searchers import SearcherGrid

--- src/slune/__init__.py
+++ src/slune/__init__.py
@@ -8,4 +8,4 @@ from .slune import submit_job, sbatchit, lsargs, garg, get_csv_slog
 from . import base, utils
 
 __all__ = ['submit_job', 'sbatchit', 'lsargs', 'garg', 'get_csv_slog',
-           'base', 'utils', 'default', 'grid', 'grid']
+           'base', 'utils', 'default', 'grid', 'csv']
---
+++ src/slune/loggers/__init__.py
@@ -0,0 +1,3 @@
+from .default import DefaultLogger
+
+__all__ = ['DefaultLogger']
